bb095a67f3adb0700be92a2f104bf5e738916ff0
Felipe Lima <felip.3lima@hotmail.com>
2020-10-05 20:50:09 -0300

    Add editorConfig

--- src/pages/Smartphone/Smartphone.styles.ts
+++ src/pages/Smartphone/Smartphone.styles.ts
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-    width: 100%;
-    padding: 0 20%;
+  width: 100%;
+  padding: 0 20%;
 `;
 
 export const Title = styled.h1`
-    font-size: 28px;
-    text-align: center;
-    margin-bottom: 30px;
-
+  font-size: 28px;
+  text-align: center;
+  margin-bottom: 30px;
 `;
--- src/pages/Smartphone/Smartphone.tsx
+++ src/pages/Smartphone/Smartphone.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  InputLabel,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from '@material-ui/core';
 import Section from '../../components/Section/Section';
-import { Button, FormControl, FormControlLabel, FormLabel, InputLabel, Radio, RadioGroup, Select, TextField } from '@material-ui/core';
 import { Form, Title } from './Smartphone.styles';
 
 // import { Container } from './styles';
@@ -11,37 +21,89 @@ const Smartphone: React.FC = () => {
       <Title>Lista de Smartphones</Title>
       <Form>
         <TextField
-          id="name" label="Nome" variant="outlined" fullWidth style={{marginBottom: 20}}
+          id="name"
+          label="Nome"
+          variant="outlined"
+          fullWidth
+          style={{ marginBottom: 20 }}
         />
 
-        <FormControl component="fieldset" fullWidth style={{marginBottom: 15}}>
+        <FormControl
+          component="fieldset"
+          fullWidth
+          style={{ marginBottom: 15 }}
+        >
           <FormLabel component="legend">Sistema Operacional</FormLabel>
           <RadioGroup aria-label="so" name="so" row>
-            <FormControlLabel value="Android" control={<Radio color="primary"/>} label="Android" />
-            <FormControlLabel value="IOS" control={<Radio color="primary" />} label="IOS" />
-            <FormControlLabel value="Windows Phone" control={<Radio color="primary" />} label="Windows Phone" />
+            <FormControlLabel
+              value="Android"
+              control={<Radio color="primary" />}
+              label="Android"
+            />
+            <FormControlLabel
+              value="IOS"
+              control={<Radio color="primary" />}
+              label="IOS"
+            />
+            <FormControlLabel
+              value="Windows Phone"
+              control={<Radio color="primary" />}
+              label="Windows Phone"
+            />
           </RadioGroup>
         </FormControl>
 
         <TextField
-          id="process" label="Processador" variant="outlined" fullWidth style={{marginBottom: 20}}
+          id="process"
+          label="Processador"
+          variant="outlined"
+          fullWidth
+          style={{ marginBottom: 20 }}
         />
 
         <TextField
-          id="screen" label="Tela" variant="outlined" fullWidth style={{marginBottom: 20}}
+          id="screen"
+          label="Tela"
+          variant="outlined"
+          fullWidth
+          style={{ marginBottom: 20 }}
         />
-        <FormControl component="fieldset" fullWidth style={{marginBottom: 15}}>
+        <FormControl
+          component="fieldset"
+          fullWidth
+          style={{ marginBottom: 15 }}
+        >
           <FormLabel component="legend">Memória RAM</FormLabel>
           <RadioGroup aria-label="memoryRam" name="gender1" row>
-            <FormControlLabel value="2" control={<Radio color="primary"/>} label="2GB" />
-            <FormControlLabel value="3" control={<Radio color="primary" />} label="3GB" />
-            <FormControlLabel value="4" control={<Radio color="primary" />} label="4GB" />
-            <FormControlLabel value="6" control={<Radio color="primary" />} label="6GB" />
-            <FormControlLabel value="8" control={<Radio color="primary" />} label="8GB" />
+            <FormControlLabel
+              value="2"
+              control={<Radio color="primary" />}
+              label="2GB"
+            />
+            <FormControlLabel
+              value="3"
+              control={<Radio color="primary" />}
+              label="3GB"
+            />
+            <FormControlLabel
+              value="4"
+              control={<Radio color="primary" />}
+              label="4GB"
+            />
+            <FormControlLabel
+              value="6"
+              control={<Radio color="primary" />}
+              label="6GB"
+            />
+            <FormControlLabel
+              value="8"
+              control={<Radio color="primary" />}
+              label="8GB"
+            />
           </RadioGroup>
         </FormControl>
 
-        <FormControl variant="outlined" fullWidth style={{marginBottom: 20}}>
+        <FormControl variant="outlined" fullWidth style={{ marginBottom: 20 }}>
           <InputLabel htmlFor="memory">Memória</InputLabel>
           <Select
             native
@@ -61,8 +123,26 @@ const Smartphone: React.FC = () => {
           </Select>
         </FormControl>
 
-        <Button variant="contained" color="primary" onClick={() => { alert('Pressed') }} style={{marginRight: 5}}>Cadastrar</Button>
-        <Button variant="outlined" color="primary" onClick={() => { alert('Pressed') }} style={{marginLeft: 5}}>Voltar</Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            alert('Pressed');
+          }}
+          style={{ marginRight: 5 }}
+        >
+          Cadastrar
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            alert('Pressed');
+          }}
+          style={{ marginLeft: 5 }}
+        >
+          Voltar
+        </Button>
       </Form>
     </Section>
   );
