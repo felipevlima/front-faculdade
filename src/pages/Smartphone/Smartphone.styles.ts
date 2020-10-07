137fc5770d275e51c81e352ebf5ac3acfeeffacd
Gustavo Cunha <gustavo.oliveira-98@hotmail.com>
2020-10-07 16:10:47 -0300

    Created the CRUD of the smartphone, only missing error correction

--- src/pages/Smartphone/Smartphone.styles.ts
+++ src/pages/Smartphone/Smartphone.styles.ts
@@ -1,12 +1,39 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  width: 100%;
-  padding: 0 20%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2);
+  grid-row-gap: 10px;
+  grid-column-gap: 25px;
+  background: #F2F2F2;
+  padding: 2.5%;
+  border-radius: 10px;
 `;
 
 export const Title = styled.h1`
   font-size: 28px;
   text-align: center;
   margin-bottom: 30px;
+  border-bottom-color: #000;
+  border-bottom-width: 1; 
+  border-bottom-style: solid;
+  padding-bottom: 5px;
+  margin: 0 100px;
+`;
+
+export const ButtonDiv = styled.div`
+  margin: 5px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NotFoundText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 70vh;
+
+  color: #333;
+  font-size: 1.6rem;
 `;
