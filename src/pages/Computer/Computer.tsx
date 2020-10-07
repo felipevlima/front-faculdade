14b1c3844d3ccf635ab0aa51c0ff6c882112bed4
Leonardo Rocha <leorocha97@gmail.com>
2020-10-07 11:05:26 -0300

    Started comp list

--- src/pages/Computer/Computer.tsx
+++ src/pages/Computer/Computer.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  Button,
+  /* FormControl,
+  FormControlLabel,
+  FormLabel,
+  InputLabel,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField, */
+} from '@material-ui/core';
+
 import Section from '../../components/Section/Section';
+import { Container, Name } from './Computer.styles';
+import api from '../../services/api';
 
 const Computer = () => {
+  const [computers, setComputers] = useState<any[]>([]);
+
+  useEffect(() => {
+    api.get('/computers').then((response) => {
+      setComputers(response.data);
+    });
+  }, []);
+
+  function handleAddComputer() {
+    console.log('funcao');
+  }
+
   return (
     <Section>
-      <h1>Computer</h1>
-      asdihasod asdihasodasdujasd asdjdsad
+      <Name>Computer List</Name>
+
+      <Button onClick={handleAddComputer}>Add computer</Button>
+
+      <Container>
+        {computers.map((computer) => (
+          <>
+            <div>{computer.model}</div>
+            <div>{computer.processor}</div>
+          </>
+        ))}
+      </Container>
     </Section>
   );
 };
